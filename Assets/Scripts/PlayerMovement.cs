4c5cebae82291a69b62c07bbbfedbc3010cf29b8
Furkan Gülhan <fgulhan07@gmail.com>
2024-06-26 19:26:16 +0300

    Improvements & New Assets
    Improvement Camera and run system
    - Camera will collide with blocks
    - Run has a timer
    New materials and model

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 
 using System.Collections.Generic;
@@ -23,12 +24,32 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 moveDirection = Vector3.zero;
     private float rotationX;
     private CharacterController characterController;
-    Vector2 rotation = Vector2.zero;
+    private Vector2 rotation = Vector2.zero;
     
     private bool canMove = true;
+
+
+    [SerializeField] private float maxRunTime = 5f;
+    public float runCooldownTime = 3.0f; // Time the player must wait before running again
+    private float runTimeRemaining;
+    private float runCooldownRemaining = 0f;
     
+    // Camera Transform Variables
+    [SerializeField] private float minDistance = 0.5f;
+    [SerializeField] private float maxDistance = 4.0f;
+    [SerializeField] private float smooth = 20.0f;
+    private Vector3 dollyDir;
+    private float distance;
+
+    private void Awake()
+    {
+        dollyDir = camera.transform.localPosition.normalized;
+        distance = camera.transform.localPosition.magnitude;
+    }
+
     void Start()
     {
+        runTimeRemaining = maxRunTime;
         characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -37,18 +58,37 @@ public class PlayerMovement : MonoBehaviour
     
 
     void Update()
-
     {
+        CameraCollision();
+        
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        
+        bool isRunning = Input.GetKey(KeyCode.LeftShift)&& runTimeRemaining > 0 && runCooldownRemaining <= 0;
+        
+        // Set current speed and direction
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
+        
+        // Running time management
+        if (isRunning)
+        {
+            runTimeRemaining -= Time.deltaTime;
+        }
+        else if (runTimeRemaining < maxRunTime)
+        {
+            runTimeRemaining += Time.deltaTime; // Recover running time if not running
+        }
+        
+        UpdateRunCooldown();
 
+        Debug.Log(runTimeRemaining);
+        
+        
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
+        // Jump
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
@@ -57,22 +97,59 @@ public class PlayerMovement : MonoBehaviour
         {
             moveDirection.y = movementDirectionY;
         }
+        
+        // Apply gravity 
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
-
-
+        
+        // Move
         characterController.Move(moveDirection * Time.deltaTime);
         
         if (canMove)
         {
             rotation.y += Input.GetAxis("Mouse X") * sensivity;
             rotation.x += -Input.GetAxis("Mouse Y") * sensivity;
-            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit); // For the rotation limit on x axis
+            
+            // Rotate the camera
             playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensivity, 0);
         }
     }
+    
+    
+    void UpdateRunCooldown()
+    {
+        if (runTimeRemaining <= 0 && runCooldownRemaining <= 0)
+        {
+            runCooldownRemaining = runCooldownTime; // Start cooldown
+        }
 
+        if (runCooldownRemaining > 0)
+        {
+            runCooldownRemaining -= Time.deltaTime;
+            if (runCooldownRemaining < 0)
+            {
+                runCooldownRemaining = 0;
+                runTimeRemaining = maxRunTime; // Reset run time after cooldown
+            }
+        }
+    }
+
+    void CameraCollision()
+    {
+        Vector3 back = camera.transform.TransformDirection(Vector3.back);
+        if (Physics.Raycast(camera.transform.position, back, out RaycastHit hit, 2))
+        {
+            distance = Mathf.Clamp((hit.distance * 0.9f), minDistance, maxDistance);
+        }
+        else
+        {
+            distance = maxDistance;
+        }
+        camera.transform.localPosition  = Vector3.Lerp(camera.transform.localPosition , dollyDir * distance, Time.deltaTime * smooth);
+        Debug.DrawRay(camera.transform.position, back * 2, Color.red);
+    }
 }
